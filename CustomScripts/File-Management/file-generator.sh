ed2b75f11d53e49ebfd7c218bd527aeafd610811
Steven G <41505038+EstenGrove@users.noreply.github.com>
2019-05-19 16:21:08 -0700

    Create file-generator.sh

---
+++ CustomScripts/File-Management/file-generator.sh
@@ -0,0 +1,5 @@
+zipf () { zip -r "$1".zip "$1" ; }          # zipf:         To create a ZIP archive of a folder
+alias numFiles='echo $(ls -1 | wc -l)'      # numFiles:     Count of non-hidden files in current dir
+alias make1mb='mkfile 1m ./1MB.dat'         # make1mb:      Creates a file of 1mb size (all zeros)
+alias make5mb='mkfile 5m ./5MB.dat'         # make5mb:      Creates a file of 5mb size (all zeros)
+alias make10mb='mkfile 10m ./10MB.dat'      # make10mb:     Creates a file of 10mb size (all zeros)
