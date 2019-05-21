e7587c09ee647be95f1970e30c56b40a1efe192b
Steven G <41505038+EstenGrove@users.noreply.github.com>
2019-05-20 20:39:49 -0700

    Update create-files-loop.sh

--- CustomScripts/File-Management/create-files-loop.sh
+++ CustomScripts/File-Management/create-files-loop.sh
@@ -16,3 +16,17 @@ for i in $(seq 10); do rm -rf "test-$i".txt; done
 
 # To create **AND** add content to a set of files
 for i in $(seq 10); do echo "File Number $i" > "test-$i".txt; done
+
+
+######## USING A NAMED LIST TO DELETE FILES FROM #########
+
+# Store the files you want to delete in a list --- NOTE: you can create files with the same method ---
+names=(test-1.txt test-2.txt test-3.txt)
+for name in "${names[@]}"
+do
+  rm -rf "$name"
+done
+
+
+# ONE LINER VERSION
+names=(test-1.txt test-2.txt test-3.txt); for name in "${names[@]}"; do rm -rf "$name"; done
