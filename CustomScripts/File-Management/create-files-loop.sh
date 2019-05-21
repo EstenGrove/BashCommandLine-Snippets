99fb4e60145bdcf0d646885beb5c6a721fdca599
Steven G <41505038+EstenGrove@users.noreply.github.com>
2019-05-20 20:02:00 -0700

    Update create-files-loop.sh

--- CustomScripts/File-Management/create-files-loop.sh
+++ CustomScripts/File-Management/create-files-loop.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-# This will create ten .txt files like: test-1.txt, test-2.txt, test-3.txt etc
+# This will create 10 blank ".txt" files with the format of: test-1.txt, test-2.txt, test-3.txt etc...
 for i in $(seq 10)
 do
   touch "test-$i".txt
 done
+
+######## ONE-LINER VERSION
+for i in $(seq 10); do touch "test-$i".txt; done
+
+
+# To delete those files
+for i in $(seq 10); do rm -rf "test-$i".txt; done
