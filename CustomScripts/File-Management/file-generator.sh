8ddb4b2e3ccf0049f698cd730a34258f584b8e3b
Steven G <41505038+EstenGrove@users.noreply.github.com>
2019-05-20 19:53:07 -0700

    Update file-generator.sh

--- CustomScripts/File-Management/file-generator.sh
+++ CustomScripts/File-Management/file-generator.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+
+
 zipf () { zip -r "$1".zip "$1" ; }          # zipf:         To create a ZIP archive of a folder
 alias numFiles='echo $(ls -1 | wc -l)'      # numFiles:     Count of non-hidden files in current dir
 alias make1mb='mkfile 1m ./1MB.dat'         # make1mb:      Creates a file of 1mb size (all zeros)
